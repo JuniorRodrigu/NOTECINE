14e3690a6c5c8d49b5bfdc8424419e5f586df309
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-10 17:05:03 -0300

    Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,21 +1,21 @@
-import { ReactComponent as GithubIcon } from './assets/img/github.svg';
+import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
 import './styles.css';
-function Navbar() {
-    return (
-        <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
-                        <div className="dsmovie-contact-container">
-                            <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
-                        </div>
-                    </a>
+function Navbar (){
+    return(
+    <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/devsuperior</p>
                 </div>
-            </nav>
-        </header>
+            </a>
+        </div>
+    </nav>
+</header>
     );
 
 }
-export default Navbar;
+export default Navbar
