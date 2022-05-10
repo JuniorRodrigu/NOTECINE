2a23a4ed81cb3dfbcd54a123c18c42c34fabf042
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-10 17:44:31 -0300

    form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,8 +1,43 @@
+import './style.css';
 function Form(){
+
+    const movie = {
+        id: 1,
+        image: "https://br.web.img3.acsta.net/pictures/19/11/29/17/57/5161763.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 return(
 
-    <h1>pagina de fomulário</h1>
-);
+
     
+    <div className="dsmovie-form-container">
+    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.image} />
+    <div className="dsmovie-card-bottom-container">
+        <h3>{movie.image}</h3>
+        <form className="dsmovie-form">
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div className="dsmovie-form-btn-container">
+                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+            </div>
+        </form >
+        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+    </div >
+</div >
+);
 }
 export default Form;
