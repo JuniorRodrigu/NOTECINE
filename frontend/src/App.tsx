8ac65961defa3b6dec1ad1725285c664a28a66a6
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-10 16:56:45 -0300

    navba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 
+import Navbar from 'components/Navbar';
+import {ReactComponent as GithubIcon} from './assets/img/github.svg';
 
 function App() {
   return (
-  <h1>home</h1>
+  <Navbar/>
   );
 }
 
