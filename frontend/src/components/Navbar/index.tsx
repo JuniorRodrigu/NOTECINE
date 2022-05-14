1825e5f486da6b73de0315dfcc5fa64a5ad24180
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-14 18:14:23 -0300

    pesu

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,11 +5,11 @@ function Navbar (){
     <header>
     <nav className="container">
         <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior">
+            <h1>hotCinema</h1>
+            <a href="https://github.com/JuniorRodrigu">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
+                    <p className="dsmovie-contact-link">/JuniorRodrigu</p>
                 </div>
             </a>
         </div>
