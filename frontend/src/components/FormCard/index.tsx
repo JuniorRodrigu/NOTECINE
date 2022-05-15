81095ac036a3b4e59014b3c6a6a7d36425033cfb
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-14 21:29:47 -0300

    useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,61 +1,95 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
+import { validateEmail } from 'utils/validate';
 
 
 
 
-    type Props={ 
-        movieId:String;
-    }
-    function FormCard({movieId}:Props){
+type Props = {
+    movieId: String;
+}
+function FormCard({ movieId }: Props) {
+    const navigate = useNavigate();
 
-        const [movie, setMovie]=useState<Movie>();
-         
-        useEffect(() =>{
-            axios.get(`${BASE_URL}/movies/${movieId}`)
-            .then(response =>{
+
+    const [movie, setMovie] = useState<Movie>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies/${movieId}`)
+            .then(response => {
                 setMovie(response.data)
-             });
+            });
 
-        },[movieId])
-
-return(
+    }, [movieId])
 
 
-    
-    <div className="dsmovie-form-container">
-    <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.image} />
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="email">Informe seu email</label>
-                <input type="email" className="form-control" id="email" />
-            </div>
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="score">Informe sua avaliação</label>
-                <select className="form-control" id="score">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div className="dsmovie-form-btn-container">
-                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-            </div>
-        </form >
-        <Link to="/">
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score =(event.target as any).score.value; 
 
 
-        </Link>
-    </div >
-</div >
-);
+        if (!validateEmail(email)){
+            return;
+
+        }
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+         axios (config).then(response=>{
+            navigate("/");
+         });                       
+
+        }
+
+
+
+    return (
+
+
+
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.image} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie?.title}</h3>
+                <form className="dsmovie-form" onSubmit={handleSubmit}>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+
+                </Link>
+            </div >
+        </div >
+    );
 }
 export default FormCard;
