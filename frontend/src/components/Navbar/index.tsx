2d1d9c8667994238a918919bd601a9f3be3afc07
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-14 22:32:53 -0300

    NOME SITE

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar (){
     <header>
     <nav className="container">
         <div className="dsmovie-nav-content">
-            <h1>hotCinema</h1>
+            <h1>NOTECINE</h1>
             <a href="https://github.com/JuniorRodrigu">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
